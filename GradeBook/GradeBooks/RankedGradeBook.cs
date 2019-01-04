bf9914c8536cb815e9baba942d0ebd43cda3b705
sinoma <m_mengqi@163.com>
2019-01-04 21:42:04 +0800

    Add files via upload

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿
 using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +9,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name):base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Students.Sort((x, y) =>x.AverageGrade.CompareTo(y.AverageGrade));
+
+            if (averageGrade >= Students[(int)Math.Ceiling(Students.Count*0.2)].AverageGrade)
+                return 'A';
+            else if (averageGrade >= Students[(int)Math.Ceiling(Students.Count * 0.4)].AverageGrade)
+                return 'B';
+            else if (averageGrade >= Students[(int)Math.Ceiling(Students.Count * 0.6)].AverageGrade)
+                return 'C';
+            else if (averageGrade >= Students[(int)Math.Ceiling(Students.Count * 0.8)].AverageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
